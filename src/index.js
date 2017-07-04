d807eae98baa4f404700fa48e6d37b44ac8b976b
David Hu <davidhu314@gmail.com>
2017-07-04 15:13:52 -0700

    create moon loader

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import BounceLoader from './spinners/BounceLoader';
 import ClipLoader from './spinners/ClipLoader';
 import DotLoader from './spinners/DotLoader';
 import FadeLoader from './spinners/FadeLoader';
+import GridLoader from './spinners/GridLoader';
+import MoonLoader from './spinners/MoonLoader';
 
 module.exports = {
   BeatLoader,
   BounceLoader,
   ClipLoader,
   DotLoader,
-  FadeLoader
+  FadeLoader,
+  GridLoader,
+  MoonLoader
 }
---
+++ src/spinners/MoonLoader.jsx
@@ -0,0 +1,124 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import assign from 'domkit/appendVendorPrefix';
+import insertKeyframesRule from 'domkit/insertKeyframesRule';
+
+/**
+ * @type {object}
+ */
+const keyframes = {
+  '100%': {
+    transform: 'rotate(360deg)'
+  }
+}
+
+/**
+ * @type {string}
+ */
+const animationName = insertKeyframesRule(keyframes);
+
+class Loader extends React.Component {
+
+  /**
+   * @param {string} size size of the ball
+   * @return {object} object with ball properties
+   */
+  getBallStyle(size) {
+    return {
+      width: size,
+      height: size,
+      borderRadius: '100%'
+    }
+  }
+
+  /**
+   * @param  {number} i element index
+   * @return {object} object with animation properties
+   */
+  getAnimationStyle(i) {
+    let animation = [animationName, '0.6s', '0s', 'infinite', 'linear'].join(' ');
+    let animationFillMode = 'forwards';
+
+    return {
+      animation: animation,
+      animationFillMode: animationFillMode
+    }
+  }
+
+  /**
+   * @param  {number} i element index
+   * @return {object} object with style properties
+   */
+  getStyle(i) {
+    var size = parseInt(this.props.size);
+    var moonSize = size / 7;
+
+    if (i == 1) {
+      return assign(
+        this.getBallStyle(moonSize), 
+        this.getAnimationStyle(i), 
+        {
+          backgroundColor: this.props.color,
+          opacity: '0.8',
+          position: 'absolute',
+          top: size / 2 - moonSize / 2
+        }
+      );
+    } else if (i == 2) {
+      return assign(
+        this.getBallStyle(size), 
+        {
+          border: moonSize + 'px solid ' + this.props.color,
+          opacity: 0.1
+        }
+      );
+    } else {
+      return assign(this.getAnimationStyle(i), { position: 'relative' });
+    }
+  }
+
+  /**
+   * @param {boolean} loading Check if loading
+   * @return {ReactComponent | null} Returns Loader or null
+   */
+  renderLoader(loading) {
+    if (loading) {
+      return (
+        <div id={this.props.id} className={this.props.className}>
+          <div style={this.getStyle(0)}>
+            <div style={this.getStyle(1)}></div>
+            <div style={this.getStyle(2)}></div>
+          </div>
+        </div>
+      );
+    }
+
+    return null;
+  }
+
+  render() {
+    return this.renderLoader(this.props.loading);
+  }
+}
+
+/**
+ * @type {object}
+ */
+Loader.propTypes = {
+  loading: PropTypes.bool,
+  color: PropTypes.string,
+  size: PropTypes.string,
+  margin: PropTypes.string
+}
+
+/**
+ * @type {object}
+ */
+Loader.defaultProps = {
+  loading: true,
+  color: '#ffffff',
+  size: '60px',
+  margin: '2px'
+}
+
+export default Loader;
