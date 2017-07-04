3647ef3f68e5bcc9111d3df3267ef1baad6cec12
David Hu <davidhu314@gmail.com>
2017-07-04 15:58:40 -0700

    fix typo in ring loader, add rotate loader

--- src/spinners/RotateLoader.jsx
+++ src/spinners/RotateLoader.jsx
@@ -6,49 +6,34 @@ import insertKeyframesRule from 'domkit/insertKeyframesRule';
 /**
  * @type {object}
  */
-let rightRotateKeyframes = {
+const keyframes = {
   '0%': {
-    transform: 'rotateX(0deg) rotateY(0deg) rotateZ(0deg)'
-
+    transform: 'rotate(0deg)'
+  },
+  '50%': {
+    transform: 'rotate(180deg)'
   },
   '100%': {
-    transform: 'rotateX(180deg) rotateY(360deg) rotateZ(360deg)'
+    transform: 'rotate(360deg)'
   }
-};
-
-/**
- * @type {object}
- */
-let leftRotateKeyframes = {
-  '0%': {
-    transform: 'rotateX(0deg) rotateY(0deg) rotateZ(0deg)'
-  },
-  '100%': {
-    transform: 'rotateX(360deg) rotateY(180deg) rotateZ(360deg)'
-  }
-};
+}
 
 /**
  * @type {string}
  */
-let rightRotateAnimationName = insertKeyframesRule(rightRotateKeyframes);
-
-/**
- * @type {string}
- */
-let leftRotateAnimationName = insertKeyframesRule(leftRotateKeyframes);
+const animationName = insertKeyframesRule(keyframes);
 
 class Loader extends React.Component {
 
    /**
    * @return {object} object with ball properties
    */
-  getCircleStyle() {
+  getBallStyle() {
     return {
-      width: size,
-      height: size,
-      border: size / 10 + 'px solid ' + this.props.color,
-      opacity: 0.4,
+      backgroundColor: this.props.color,
+      width: this.props.size,
+      height: this.props.size,
+      margin: this.props.margin,
       borderRadius: '100%'
     }
   }
@@ -58,12 +43,10 @@ class Loader extends React.Component {
    * @return {object} object with animation properties
    */
   getAnimationStyle(i) {
-    let animation = [i == 1 ? rightRotateAnimationName : leftRotateAnimationName, '2s', '0s', 'infinite', 'linear'].join(' ');
-    let animationFillMode = 'forwards';
-    let perspective = '800px';
+    let animation = [animationName, '1s', '0s', 'infinite', 'cubic-bezier(.7,-.13,.22,.86)'].join(' ');
+    let animationFillMode = 'both';
 
     return {
-      perspective: perspective,
       animation: animation,
       animationFillMode: animationFillMode
     }
@@ -74,25 +57,26 @@ class Loader extends React.Component {
    * @return {object} object with style properties
    */
   getStyle(i) {
-    var size = parseInt(this.props.size);
-
     if (i) {
       return assign(
-        this.getCircleStyle(size), 
-        this.getAnimationStyle(i), 
+        this.getBallStyle(i), 
         {
+          opacity: '0.8',
           position: 'absolute',
           top: 0,
-          left: 0
+          left: i % 2 ? -28 : 25
         }
       );
     }
 
-    return {
-      width: size, 
-      height: size, 
-      position: 'relative'
-    };
+    return assign(
+      this.getBallStyle(i), 
+      this.getAnimationStyle(i), 
+      {
+        display: 'inline-block',
+        position: 'relative'
+      }
+    );
   }
 
   /**
@@ -103,7 +87,7 @@ class Loader extends React.Component {
     if (loading) {
       return (
         <div id={this.props.id} className={this.props.className}>
-          <div style={this.getStyle(0)}>
+          <div style={this.getStyle()}>
             <div style={this.getStyle(1)} />
             <div style={this.getStyle(2)} />
           </div>
@@ -135,7 +119,7 @@ Loader.propTypes = {
 Loader.defaultProps = {
   loading: true,
   color: '#ffffff',
-  size: '60px',
+  size: '15px',
   margin: '2px'
 }
 
