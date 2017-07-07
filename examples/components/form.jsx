11a39a3c0a9ae1e7125537a99d33e2cce60542f2
David Hu <davidhu314@gmail.com>
2017-07-07 12:30:57 -0700

    add form to edit sizes and other loader attributes

---
+++ examples/components/form.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Form = ({ inputs, update }) => (
+  <div className='spinner-form'>
+    { Object.keys(inputs).map(name => (
+      <div className='spinner-form-input'>
+        <input
+          name={name}
+          type='number'
+          value={inputs[name]}
+          onChange={update(name)}
+        />
+        <span className={`bar`} />
+        <label htmlFor={name}>
+          { name }
+        </label>
+      </div>
+    ))}
+  </div>
+);
+
+Form.propTypes = {
+  inputs: PropTypes.shape().isRequired,
+  update: PropTypes.func.isRequired
+};
+
+export { Form };
--- examples/index.jsx
+++ examples/index.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Code, ColorPicker } from './components';
+import { Code, ColorPicker, LoaderItem } from './components';
 import Spinners from '../src';
 
 class SpinnerExamples extends React.Component {
@@ -11,25 +11,22 @@ class SpinnerExamples extends React.Component {
     this.state = {
       color: '#36D7B7',
       showPicker: false
-    }
+    };
 
     this.updateColor = this.updateColor.bind(this);
     this.togglePicker = this.togglePicker.bind(this);
-
   }
 
   componentDidMount() {
-
     document.addEventListener('scroll', () => {
       let picker = document.getElementsByClassName('color-picker')[0];
-      let top = 370 - window.scrollY * 2;
+      let top = 370 - (window.scrollY * 2);
       if (top > 60) {
         picker.style.top = `${top}px`;
       } else {
         picker.style.top = `50px`;
       }
     });
-
   }
 
   updateColor(color) {
@@ -61,10 +58,7 @@ class SpinnerExamples extends React.Component {
         </div>
 
         { Object.keys(Spinners).map(name => (
-          <div className="spinner-item">
-            <div className="spinner-title">{name}</div>
-            { this.renderSpinner(Spinners[name]) }
-          </div>
+          <LoaderItem color={color} name={name} spinner={Spinners[name]} />
         )) }
       </div>
     )
