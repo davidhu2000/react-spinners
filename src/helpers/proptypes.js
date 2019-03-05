36eab7abed52ed832d3a48adf427db54c518ecd7
David Hu <davidhu314@gmail.com>
2019-03-04 21:09:52 -0800

    reorganize proptypes helpers

--- src/helpers/proptypes.js
+++ src/helpers/proptypes.js
@@ -1,6 +1,11 @@
 import PropTypes from 'prop-types';
 
-// list of string constants to represent different props
+// 
+/*
+ *
+ * List of string constants to represent different props
+ *
+ */
 const LOADING = 'loading';
 const COLOR = 'color';
 const CSS = 'css';
@@ -14,6 +19,28 @@ const RADIUS = 'radius';
 const RADIUS_UNIT = 'radiusUnit';
 const MARGIN = 'margin';
 
+/*
+ *
+ * Array for onlyUpdateForKeys function
+ *
+ */
+const commonStrings = [LOADING, COLOR, CSS];
+const sizeStrings = [SIZE, SIZE_UNIT];
+const heightWidthString = [HEIGHT, HEIGHT_UNIT, WIDTH, WIDTH_UNIT];
+
+export const sizeKeys = commonStrings.concat(sizeStrings);
+export const sizeMarginKeys = sizeKeys.concat([MARGIN]);
+export const heightWidthKeys = commonStrings.contact(heightWidthString);
+export const heightWidthRadiusKeys = heightWidthKeys.concat([
+  RADIUS, RADIUS_UNIT, MARGIN
+]);
+
+
+/*
+ *
+ * PropType object for different loaders
+ *
+ */
 const common = {
   [LOADING]: PropTypes.bool,
   [COLOR]: PropTypes.string,
@@ -23,25 +50,16 @@ const common = {
   })
 };
 
-const commonValues = {
-  [LOADING]: true,
-  [COLOR]: '#000000',
-  [CSS]: {}
-};
-
-const commonStrings = [LOADING, COLOR, CSS];
-
 const size = {
   [SIZE]: PropTypes.number,
   [SIZE_UNIT]: PropTypes.string
 };
 
-const sizeValues = sizeValue => ({
-  [SIZE]: sizeValue,
-  [SIZE_UNIT]: 'px'
-});
-
-const sizeStrings = [SIZE, SIZE_UNIT];
+const commonValues = {
+  [LOADING]: true,
+  [COLOR]: '#000000',
+  [CSS]: {}
+};
 
 const heightWidth = {
   [WIDTH]: PropTypes.number,
@@ -50,6 +68,27 @@ const heightWidth = {
   [HEIGHT_UNIT]: PropTypes.string
 };
 
+export const sizeProps = Object.assign({}, common, size);
+
+export const sizeMarginProps = Object.assign({}, sizeProps, {
+  [MARGIN]: PropTypes.string
+});
+
+export const heightWidthProps = Object.assign({}, common, heightWidth);
+
+export const heightWidthRadiusProps = Object.assign({}, heightWidthProps, {
+  [RADIUS]: PropTypes.number,
+  [RADIUS_UNIT]: PropTypes.string,
+  [MARGIN]: PropTypes.string
+});
+
+
+/*
+ *
+ * DefaultProps object for different loaders
+ *
+ */
+
 const heightWidthValues = (height, width) => ({
   [HEIGHT]: height,
   [HEIGHT_UNIT]: 'px',
@@ -57,19 +96,16 @@ const heightWidthValues = (height, width) => ({
   [WIDTH_UNIT]: 'px'
 });
 
-const heightWidthString = [HEIGHT, HEIGHT_UNIT, WIDTH, WIDTH_UNIT];
 
-export const sizeProps = Object.assign({}, common, size);
+const sizeValues = sizeValue => ({
+  [SIZE]: sizeValue,
+  [SIZE_UNIT]: 'px'
+});
+
 
 export const sizeDefaults = sizeValue => {
   return Object.assign({}, commonValues, sizeValues(sizeValue));
 };
-
-export const sizeKeys = commonStrings.concat(sizeStrings);
-
-export const sizeMarginProps = Object.assign({}, sizeProps, {
-  [MARGIN]: PropTypes.string
-});
 
 export const sizeMarginDefaults = sizeValue => {
   return Object.assign({}, sizeValues(sizeValue), {
@@ -77,21 +113,9 @@ export const sizeMarginDefaults = sizeValue => {
   });
 };
 
-export const sizeMarginKeys = sizeKeys.concat([MARGIN]);
-
-export const heightWidthProps = Object.assign({}, common, heightWidth);
-
 export const heightWidthDefaults = (height, width) => {
   return Object.assign({}, commonValues, heightWidthValues(height, width));
 };
-
-export const heightWidthKeys = commonStrings.contact(heightWidthString)
-
-export const heightWidthRadiusProps = Object.assign({}, heightWidthProps, {
-  [RADIUS]: PropTypes.number,
-  [RADIUS_UNIT]: PropTypes.string,
-  [MARGIN]: PropTypes.string
-});
 
 export const heightWidthRadiusValues = (height, width, radius = 2) => {
   return Object.assign({}, heightWidthDefaults(height, width), {
@@ -101,6 +125,4 @@ export const heightWidthRadiusValues = (height, width, radius = 2) => {
   });
 };
 
-export const heightWidthRadiusKeys = commonStrings.contact(heightWidthString).concat([
-  RADIUS, RADIUS_UNIT, MARGIN
-]);
+
