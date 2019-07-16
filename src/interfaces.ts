b54a55c8e5f7e2b11c11e0ceedabfa37fcee11fb
David Hu <davidhu314@gmail.com>
2019-07-15 21:25:11 -0700

    convert barloader to typescript

---
+++ src/interfaces.ts
@@ -0,0 +1,19 @@
+export interface PrecompiledCss {
+  name: string;
+  styles: string;
+}
+
+export type StyleFunction = (i?: number) => PrecompiledCss;
+
+interface CommonProps {
+  color?: string;
+  loading?: boolean;
+  css?: string | PrecompiledCss;
+}
+
+export interface BarLoaderProps extends CommonProps {
+  height?: number;
+  heightUnit?: string;
+  width?: number;
+  widthUnit?: string;
+}
