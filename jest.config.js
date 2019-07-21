d30f6a8fd2a75916cd5681c90b414fe3303a7573
David Hu <davidhu314@gmail.com>
2019-07-20 17:59:07 -0700

    set up jest-emotion

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,6 @@ module.exports = {
   roots: ["<rootDir>"],
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(t|j)sx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  snapshotSerializers: ["enzyme-to-json/serializer"],
+  snapshotSerializers: ["enzyme-to-json/serializer", "jest-emotion"],
   setupFilesAfterEnv: ["<rootDir>/__tests__/config/enzyme.ts"]
 };
