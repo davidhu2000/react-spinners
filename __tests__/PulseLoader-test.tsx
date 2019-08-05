9c400dae40e901f4e495c1e803be65a7cc53b6a8
David Hu <davidhu314@gmail.com>
2019-08-04 21:37:24 -0700

    add pulseloader tests

---
+++ __tests__/PulseLoader-test.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import { mount, ReactWrapper } from "enzyme";
+import { matchers } from "jest-emotion";
+expect.extend(matchers);
+
+import PulseLoader from "../src/PulseLoader";
+import { LoaderSizeMarginProps } from "../src/interfaces";
+import { sizeMarginDefaults } from "../src/helpers";
+
+describe("PulseLoader", () => {
+  let loader: ReactWrapper;
+  let props: LoaderSizeMarginProps;
+
+  it("should match snapshot", () => {
+    loader = mount(<PulseLoader />);
+    expect(loader).toMatchSnapshot();
+  });
+
+  it("should contain default props if no props are passed", () => {
+    props = loader.props();
+    expect(props).toEqual(sizeMarginDefaults(15));
+  });
+
+  it("should contain styles created using default props", () => {
+    for (let i: number = 0; i < 3; i++) {
+      expect(loader.find("div div").at(i)).toHaveStyleRule("background-color", "#000000");
+      expect(loader.find("div div").at(i)).toHaveStyleRule("height", "15px");
+      expect(loader.find("div div").at(i)).toHaveStyleRule("width", "15px");
+      expect(loader.find("div div").at(i)).toHaveStyleRule("margin", "2px");
+    }
+  });
+
+  it("should render null if loading prop is set as false", () => {
+    loader = mount(<PulseLoader loading={false} />);
+    expect(loader.isEmptyRender()).toBe(true);
+  });
+
+  it("renders the correct color based on prop", () => {
+    loader = mount(<PulseLoader color="#e2e2e2" />);
+    for (let i: number = 0; i < 3; i++) {
+      expect(loader.find("div div").at(i)).not.toHaveStyleRule("background-color", "#000000");
+      expect(loader.find("div div").at(i)).toHaveStyleRule("background-color", "#e2e2e2");
+    }
+  });
+
+  it("renders the correct size for the parent div based on props", () => {
+    loader = mount(<PulseLoader size={18} />);
+
+    for (let i: number = 0; i < 3; i++) {
+      expect(loader.find("div div").at(i)).not.toHaveStyleRule("height", "15px");
+      expect(loader.find("div div").at(i)).not.toHaveStyleRule("width", "15px");
+
+      expect(loader.find("div div").at(i)).toHaveStyleRule("height", "18px");
+      expect(loader.find("div div").at(i)).toHaveStyleRule("width", "18px");
+    }
+  });
+
+  it("renders the css override based on props", () => {
+    loader = mount(<PulseLoader css={"position: fixed; width: 100px; color: blue;"} />);
+    expect(loader).toHaveStyleRule("position", "fixed");
+    expect(loader).toHaveStyleRule("width", "100px");
+    expect(loader).toHaveStyleRule("color", "blue");
+  });
+});
