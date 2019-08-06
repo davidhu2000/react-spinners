3e6df7326f601aa961238f46f765f333a75c0009
David Hu <davidhu314@gmail.com>
2019-08-05 21:33:56 -0700

    fix beatloader tests

--- __tests__/BeatLoader-tests.tsx
+++ __tests__/BeatLoader-tests.tsx
@@ -10,6 +10,9 @@ import { sizeMarginDefaults } from "../src/helpers";
 describe("BeatLoader", () => {
   let loader: ReactWrapper;
   let props: LoaderSizeMarginProps;
+  let defaultColor: string = "#000000";
+  let defaultSize: number = 15;
+  let defaultUnit: string = "px";
 
   it("should match snapshot", () => {
     loader = mount(<BeatLoader />);
@@ -18,15 +21,15 @@ describe("BeatLoader", () => {
 
   it("should contain default props if no props are passed", () => {
     props = loader.props();
-    expect(props).toEqual(sizeMarginDefaults(15));
+    expect(props).toEqual(sizeMarginDefaults(defaultSize));
   });
 
   it("should contain styles created using default props", () => {
     props = loader.props();
-    expect(loader.find("div div")).toHaveStyleRule("height", "15px");
-    expect(loader.find("div div")).toHaveStyleRule("width", "15px");
+    expect(loader.find("div div")).toHaveStyleRule("height", `${defaultSize}${defaultUnit}`);
+    expect(loader.find("div div")).toHaveStyleRule("width", `${defaultSize}${defaultUnit}`);
     expect(loader.find("div div")).toHaveStyleRule("margin", "2px");
-    expect(loader.find("div div")).toHaveStyleRule("background-color", "#000000");
+    expect(loader.find("div div")).toHaveStyleRule("background-color", defaultColor);
   });
 
   it("should render null if loading prop is set as false", () => {
@@ -34,35 +37,37 @@ describe("BeatLoader", () => {
     expect(loader.isEmptyRender()).toBe(true);
   });
 
-  it("renders the correct color based on prop", () => {
+  it("should render the correct color based on prop", () => {
     loader = mount(<BeatLoader color="#e2e2e2" />);
-    expect(loader.find("div div")).not.toHaveStyleRule("background-color", "#000000");
+    expect(loader.find("div div")).not.toHaveStyleRule("background-color", defaultColor);
     expect(loader.find("div div")).toHaveStyleRule("background-color", "#e2e2e2");
   });
 
-  it("renders the correct size based on props", () => {
+  it("should render the correct size based on props", () => {
+    let size: number = 18;
     loader = mount(<BeatLoader size={18} />);
-    expect(loader.find("div div")).not.toHaveStyleRule("height", "15px");
-    expect(loader.find("div div")).not.toHaveStyleRule("width", "15px");
-    expect(loader.find("div div")).toHaveStyleRule("height", "18px");
-    expect(loader.find("div div")).toHaveStyleRule("width", "18px");
+    expect(loader.find("div div")).not.toHaveStyleRule("height", `${defaultSize}${defaultUnit}`);
+    expect(loader.find("div div")).not.toHaveStyleRule("width", `${defaultSize}${defaultUnit}`);
+    expect(loader.find("div div")).toHaveStyleRule("height", `${size}${defaultUnit}`);
+    expect(loader.find("div div")).toHaveStyleRule("width", `${size}${defaultUnit}`);
   });
 
-  it("renders the correct heightUnit basd on props", () => {
+  it("should render the correct heightUnit basd on props", () => {
+    let unit: string = "%";
     loader = mount(<BeatLoader sizeUnit="%" />);
-    expect(loader.find("div div")).not.toHaveStyleRule("height", "15px");
-    expect(loader.find("div div")).not.toHaveStyleRule("width", "15px");
-    expect(loader.find("div div")).toHaveStyleRule("height", "15%");
-    expect(loader.find("div div")).toHaveStyleRule("width", "15%");
+    expect(loader.find("div div")).not.toHaveStyleRule("height", `${defaultSize}${defaultUnit}`);
+    expect(loader.find("div div")).not.toHaveStyleRule("width", `${defaultSize}${defaultUnit}`);
+    expect(loader.find("div div")).toHaveStyleRule("height", `${defaultSize}${unit}`);
+    expect(loader.find("div div")).toHaveStyleRule("width", `${defaultSize}${unit}`);
   });
 
-  it("renders the correct margin props based on props", () => {
+  it("should render the correct margin props based on props", () => {
     loader = mount(<BeatLoader margin="4%" />);
     expect(loader.find("div div")).not.toHaveStyleRule("margin", "2px");
     expect(loader.find("div div")).toHaveStyleRule("margin", "4%");
   });
 
-  it("renders the css override based on props", () => {
+  it("should render the css override based on props", () => {
     loader = mount(<BeatLoader css={"position: absolute; overflow: scroll;"} />);
     expect(loader).toHaveStyleRule("position", "absolute");
     expect(loader).toHaveStyleRule("overflow", "scroll");
