3feeb1748b66171b3560a85a68249d713d8b58fd
David Hu <davidhu314@gmail.com>
2019-08-05 22:28:40 -0700

    update FadeLoader tests

--- __tests__/FadeLoader-tests.tsx
+++ __tests__/FadeLoader-tests.tsx
@@ -10,6 +10,11 @@ import { heightWidthRadiusDefaults } from "../src/helpers";
 describe("FadeLoader", () => {
   let loader: ReactWrapper;
   let props: LoaderHeightWidthRadiusProps;
+  let defaultColor: string = "#000000";
+  let defaultHeight: number = 15;
+  let defaultWidth: number = 5;
+  let defaultRadius: number = 2;
+  let defaultUnit: string = "px";
 
   it("should match snapshot", () => {
     loader = mount(<FadeLoader />);
@@ -18,23 +23,25 @@ describe("FadeLoader", () => {
 
   it("should contain default props if no props are passed", () => {
     props = loader.props();
-    expect(props).toEqual(heightWidthRadiusDefaults(15, 5, 2));
+    expect(props).toEqual(heightWidthRadiusDefaults(defaultHeight, defaultWidth, defaultRadius));
   });
 
-  it("parent div should contain styles created using default props", () => {
-    expect(loader).toHaveStyleRule("top", "20px");
-    expect(loader).toHaveStyleRule("left", "20px");
-    expect(loader).toHaveStyleRule("height", "60px");
-    expect(loader).toHaveStyleRule("width", "60px");
-  });
-
-  it("child div should contain styles created using default props", () => {
+  it("should contain styles created using default props", () => {
     for (let i: number = 0; i < 8; i++) {
-      expect(loader.find("div div").at(i)).toHaveStyleRule("background-color", "#000000");
-      expect(loader.find("div div").at(i)).toHaveStyleRule("height", "15px");
-      expect(loader.find("div div").at(i)).toHaveStyleRule("width", "5px");
+      expect(loader.find("div div").at(i)).toHaveStyleRule("background-color", defaultColor);
+      expect(loader.find("div div").at(i)).toHaveStyleRule(
+        "height",
+        `${defaultHeight}${defaultUnit}`
+      );
+      expect(loader.find("div div").at(i)).toHaveStyleRule(
+        "width",
+        `${defaultWidth}${defaultUnit}`
+      );
       expect(loader.find("div div").at(i)).toHaveStyleRule("margin", "2px");
-      expect(loader.find("div div").at(i)).toHaveStyleRule("border-radius", "2px");
+      expect(loader.find("div div").at(i)).toHaveStyleRule(
+        "border-radius",
+        `${defaultRadius}${defaultUnit}`
+      );
     }
   });
 
@@ -43,23 +50,103 @@ describe("FadeLoader", () => {
     expect(loader.isEmptyRender()).toBe(true);
   });
 
-  it("renders the correct color based on prop", () => {
+  it("should render the correct color based on prop", () => {
     loader = mount(<FadeLoader color="#e2e2e2" />);
-    expect(loader.find("div div")).not.toHaveStyleRule("background-color", "#000000");
+    expect(loader.find("div div")).not.toHaveStyleRule("background-color", defaultColor);
     expect(loader.find("div div")).toHaveStyleRule("background-color", "#e2e2e2");
   });
 
-  it("renders the correct size for the parent div based on props", () => {
-    loader = mount(<FadeLoader height={18} width={20} radius={5} />);
-    expect(loader.find("div div")).not.toHaveStyleRule("height", "15px");
-    expect(loader.find("div div")).not.toHaveStyleRule("width", "5px");
-    expect(loader.find("div div")).not.toHaveStyleRule("border-radius", "2px");
-    expect(loader.find("div div")).toHaveStyleRule("height", "18px");
-    expect(loader.find("div div")).toHaveStyleRule("width", "20px");
-    expect(loader.find("div div")).toHaveStyleRule("border-radius", "5px");
+  it("should render the correct height based on props", () => {
+    let height: number = 18;
+    loader = mount(<FadeLoader height={height} />);
+
+    for (let i: number = 0; i < 8; i++) {
+      expect(loader.find("div div").at(i)).not.toHaveStyleRule(
+        "height",
+        `${defaultHeight}${defaultUnit}`
+      );
+
+      expect(loader.find("div div").at(i)).toHaveStyleRule("height", `${height}${defaultUnit}`);
+    }
   });
 
-  it("renders the css override based on props", () => {
+  it("should render the correct heightUnit based on props", () => {
+    let unit: string = "%";
+    loader = mount(<FadeLoader heightUnit={unit} />);
+
+    for (let i: number = 0; i < 8; i++) {
+      expect(loader.find("div div").at(i)).not.toHaveStyleRule(
+        "height",
+        `${defaultHeight}${defaultUnit}`
+      );
+
+      expect(loader.find("div div").at(i)).toHaveStyleRule("height", `${defaultHeight}${unit}`);
+    }
+  });
+
+  it("should render the correct width based on props", () => {
+    let width: number = 20;
+    loader = mount(<FadeLoader width={width} />);
+
+    for (let i: number = 0; i < 8; i++) {
+      expect(loader.find("div div").at(i)).not.toHaveStyleRule(
+        "width",
+        `${defaultWidth}${defaultUnit}`
+      );
+
+      expect(loader.find("div div").at(i)).toHaveStyleRule("width", `${width}${defaultUnit}`);
+    }
+  });
+
+  it("should render the correct widthUnit based on props", () => {
+    let unit: string = "%";
+    loader = mount(<FadeLoader widthUnit={unit} />);
+
+    for (let i: number = 0; i < 8; i++) {
+      expect(loader.find("div div").at(i)).not.toHaveStyleRule(
+        "width",
+        `${defaultWidth}${defaultUnit}`
+      );
+
+      expect(loader.find("div div").at(i)).toHaveStyleRule("width", `${defaultWidth}${unit}`);
+    }
+  });
+
+  it("should render the correct radius based on props", () => {
+    let radius: number = 8;
+    loader = mount(<FadeLoader radius={radius} />);
+
+    for (let i: number = 0; i < 8; i++) {
+      expect(loader.find("div div").at(i)).not.toHaveStyleRule(
+        "border-radius",
+        `${defaultRadius}${defaultUnit}`
+      );
+
+      expect(loader.find("div div").at(i)).toHaveStyleRule(
+        "border-radius",
+        `${radius}${defaultUnit}`
+      );
+    }
+  });
+
+  it("should render the correct radiusUnit based on props", () => {
+    let unit: string = "%";
+    loader = mount(<FadeLoader radiusUnit={unit} />);
+
+    for (let i: number = 0; i < 8; i++) {
+      expect(loader.find("div div").at(i)).not.toHaveStyleRule(
+        "radius",
+        `${defaultRadius}${defaultUnit}`
+      );
+
+      expect(loader.find("div div").at(i)).toHaveStyleRule(
+        "border-radius",
+        `${defaultRadius}${unit}`
+      );
+    }
+  });
+
+  it("should render the css override based on props", () => {
     loader = mount(
       <FadeLoader css={"position: absolute; width: 100px; height: 200px; color: blue;"} />
     );
