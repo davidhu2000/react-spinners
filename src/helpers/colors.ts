2e54581c1fb6ff05bbc9e3890954fc5303cc0eed
David Hu <davidhu314@gmail.com>
2019-08-06 22:17:57 -0700

    add support for basic colors for BarLoader and HashLoader

--- src/helpers/colors.ts
+++ src/helpers/colors.ts
@@ -1,6 +1,30 @@
+enum BasicColors {
+  maroon = "#800000",
+  red = "#FF0000",
+  orange = "#FFA500",
+  yellow = "#FFFF00",
+  olive = "#808000",
+  green = "#008000",
+  purple = "#800080",
+  fuchsia = "#FF00FF",
+  lime = "#00FF00",
+  teal = "#008080",
+  aqua = "#00FFFF",
+  blue = "#0000FF",
+  navy = "#000080",
+  black = "#000000",
+  gray = "#808080",
+  silver = "#C0C0C0",
+  white = "#FFFFFF"
+}
+
 type RgbaFunction = (color: string, opacity: number) => string;
 
 export const calculateRgba: RgbaFunction = (color: string, opacity: number): string => {
+  if (Object.keys(BasicColors).includes(color)) {
+    color = BasicColors[color as keyof typeof BasicColors];
+  }
+
   if (color[0] === "#") {
     color = color.slice(1);
   }
