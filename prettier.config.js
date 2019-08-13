c7300b93c95953c6493ee558e63f7418c6f8fe47
David Hu <davidhu314@gmail.com>
2019-08-12 21:52:10 -0700

    prettier changes on prettier.config.js

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  trailingComma: 'none',
+  trailingComma: "none",
   tabWidth: 2,
   useTabs: false,
   semi: true,
   printWidth: 100,
   bracketSpacing: true,
   jsxBracketSameLine: false,
-  arrowParens: 'always'
+  arrowParens: "always"
 };
