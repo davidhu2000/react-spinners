a381811e613c20dc8ce492321f452841ae8047c3
David Hu <davidhu314@gmail.com>
2020-12-30 15:14:16 -0800

    fix jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -11,6 +11,6 @@ module.exports = {
   roots: ["<rootDir>"],
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(t|j)sx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  snapshotSerializers: ["enzyme-to-json/serializer", "jest-emotion"],
+  snapshotSerializers: ["@emotion/jest/serializer", "enzyme-to-json/serializer"],
   setupFilesAfterEnv: ["<rootDir>/__tests__/config/enzyme.ts"]
 };
