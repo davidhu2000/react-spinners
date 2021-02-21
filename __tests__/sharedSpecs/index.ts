7a8ef923093c42c06f9b96120e0f9436cd316e38
David Hu <davidhu314@gmail.com>
2021-02-21 14:16:02 -0800

    create shared length spec (#370)

--- __tests__/sharedSpecs/index.ts
+++ __tests__/sharedSpecs/index.ts
@@ -1,3 +1,4 @@
 export { default as commonSpecs } from "./commonSpecs";
 export { default as cssSpecs } from "./cssSpecs";
+export { default as lengthSpecs } from "./lengthSpecs";
 export { default as speedMultiplierSpecs } from "./speedMultiplierSpecs";
