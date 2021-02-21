246a0bb1df8041c93ea457a646c0e0cdd0923ddf
David Hu <davidhu314@gmail.com>
2021-02-21 14:23:32 -0800

    Refactors BounceLoader tests to used shared specs

--- __tests__/BounceLoader-tests.tsx
+++ __tests__/BounceLoader-tests.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { mount } from "enzyme";
+import { mount, ReactWrapper } from "enzyme";
 import { matchers } from "@emotion/jest";
 expect.extend(matchers);
 
 import BounceLoader from "../src/BounceLoader";
 import { sizeDefaults } from "../src/helpers";
-import { commonSpecs } from "./sharedSpecs";
+import { commonSpecs, cssSpecs, lengthSpecs } from "./sharedSpecs";
 
 describe("BounceLoader", () => {
   const defaultColor = "#000000";
@@ -13,6 +13,7 @@ describe("BounceLoader", () => {
   const defaultUnit = "px";
 
   commonSpecs(BounceLoader, sizeDefaults(defaultSize));
+  cssSpecs(BounceLoader);
 
   it("should contain styles created using default props", () => {
     const loader = mount(<BounceLoader />);
@@ -30,65 +31,16 @@ describe("BounceLoader", () => {
     expect(loader.find("span span")).toHaveStyleRule("background-color", color);
   });
 
-  describe("size prop", () => {
-    it("should render the size with px unit when size is a number", () => {
-      const size = 18;
-      const loader = mount(<BounceLoader size={size} />);
-      expect(loader).not.toHaveStyleRule("height", `${defaultSize}${defaultUnit}`);
-      expect(loader).not.toHaveStyleRule("width", `${defaultSize}${defaultUnit}`);
-      expect(loader.find("span span")).not.toHaveStyleRule(
-        "height",
-        `${defaultSize}${defaultUnit}`
-      );
-      expect(loader.find("span span")).not.toHaveStyleRule("width", `${defaultSize}${defaultUnit}`);
+  const sizeExpectStatements = (loader: ReactWrapper, length: number, unit?: string) => {
+    expect(loader).not.toHaveStyleRule("height", `${defaultSize}${defaultUnit}`);
+    expect(loader).not.toHaveStyleRule("width", `${defaultSize}${defaultUnit}`);
+    expect(loader.find("span span")).not.toHaveStyleRule("height", `${defaultSize}${defaultUnit}`);
+    expect(loader.find("span span")).not.toHaveStyleRule("width", `${defaultSize}${defaultUnit}`);
 
-      expect(loader).toHaveStyleRule("height", `${size}${defaultUnit}`);
-      expect(loader).toHaveStyleRule("width", `${size}${defaultUnit}`);
-      expect(loader.find("span span")).toHaveStyleRule("height", `${size}${defaultUnit}`);
-      expect(loader.find("span span")).toHaveStyleRule("width", `${size}${defaultUnit}`);
-    });
-
-    it("should render the size as is when size is a string with valid css unit", () => {
-      const size = "18px";
-      const loader = mount(<BounceLoader size={size} />);
-      expect(loader).not.toHaveStyleRule("height", `${defaultSize}${defaultUnit}`);
-      expect(loader).not.toHaveStyleRule("width", `${defaultSize}${defaultUnit}`);
-      expect(loader.find("span span")).not.toHaveStyleRule(
-        "height",
-        `${defaultSize}${defaultUnit}`
-      );
-      expect(loader.find("span span")).not.toHaveStyleRule("width", `${defaultSize}${defaultUnit}`);
-
-      expect(loader).toHaveStyleRule("height", `${size}`);
-      expect(loader).toHaveStyleRule("width", `${size}`);
-      expect(loader.find("span span")).toHaveStyleRule("height", `${size}`);
-      expect(loader.find("span span")).toHaveStyleRule("width", `${size}`);
-    });
-
-    it("should render the size with default unit of px when the unit is incorrect", () => {
-      const length = 18;
-      const unit = "ad";
-      const size = `${length}${unit}`;
-      const loader = mount(<BounceLoader size={size} />);
-      expect(loader).not.toHaveStyleRule("height", `${defaultSize}${defaultUnit}`);
-      expect(loader).not.toHaveStyleRule("width", `${defaultSize}${defaultUnit}`);
-      expect(loader.find("span span")).not.toHaveStyleRule(
-        "height",
-        `${defaultSize}${defaultUnit}`
-      );
-      expect(loader.find("span span")).not.toHaveStyleRule("width", `${defaultSize}${defaultUnit}`);
-
-      expect(loader).toHaveStyleRule("height", `${length}${defaultUnit}`);
-      expect(loader).toHaveStyleRule("width", `${length}${defaultUnit}`);
-      expect(loader.find("span span")).toHaveStyleRule("height", `${length}${defaultUnit}`);
-      expect(loader.find("span span")).toHaveStyleRule("width", `${length}${defaultUnit}`);
-    });
-  });
-
-  it("should render the css override based on props", () => {
-    const loader = mount(<BounceLoader css={"position: absolute; overflow: scroll;"} />);
-    expect(loader).not.toHaveStyleRule("position", "relative");
-    expect(loader).toHaveStyleRule("position", "absolute");
-    expect(loader).toHaveStyleRule("overflow", "scroll");
-  });
+    expect(loader).toHaveStyleRule("height", `${length}${unit || defaultUnit}`);
+    expect(loader).toHaveStyleRule("width", `${length}${unit || defaultUnit}`);
+    expect(loader.find("span span")).toHaveStyleRule("height", `${length}${unit || defaultUnit}`);
+    expect(loader.find("span span")).toHaveStyleRule("width", `${length}${unit || defaultUnit}`);
+  };
+  lengthSpecs(BounceLoader, "size", sizeExpectStatements);
 });
