3dda072213243ea5cfd5b19eeba468a4ea2f1d11
David Hu <davidhu314@gmail.com>
2021-02-21 20:28:16 -0800

    Refactor to use shared spec: PulseLoader

--- __tests__/PulseLoader-test.tsx
+++ __tests__/PulseLoader-test.tsx
@@ -5,7 +5,7 @@ expect.extend(matchers);
 
 import PulseLoader from "../src/PulseLoader";
 import { sizeMarginDefaults } from "../src/helpers";
-import { commonSpecs, speedMultiplierSpecs } from "./sharedSpecs";
+import { commonSpecs, cssSpecs, lengthSpecs, speedMultiplierSpecs } from "./sharedSpecs";
 
 describe("PulseLoader", () => {
   const defaultSize = 15;
@@ -16,6 +16,7 @@ describe("PulseLoader", () => {
   const defaultDelay = 0.12;
 
   commonSpecs(PulseLoader, sizeMarginDefaults(defaultSize));
+  cssSpecs(PulseLoader);
 
   it("should contain styles created using default props", () => {
     const loader = mount(<PulseLoader />);
@@ -45,109 +46,42 @@ describe("PulseLoader", () => {
     }
   });
 
-  describe("size props", () => {
-    it("should render the size with px unit when size is a number", () => {
-      const size = 18;
-      const loader = mount(<PulseLoader size={size} />);
+  const sizeExpectStatements = (loader: ReactWrapper, length: number, unit?: string) => {
+    for (let i = 0; i < 3; i++) {
+      expect(loader.find("span span").at(i)).not.toHaveStyleRule(
+        "height",
+        `${defaultSize}${defaultUnit}`
+      );
+      expect(loader.find("span span").at(i)).not.toHaveStyleRule(
+        "width",
+        `${defaultSize}${defaultUnit}`
+      );
 
-      for (let i = 0; i < 3; i++) {
-        expect(loader.find("span span").at(i)).not.toHaveStyleRule(
-          "height",
-          `${defaultSize}${defaultUnit}`
-        );
-        expect(loader.find("span span").at(i)).not.toHaveStyleRule(
-          "width",
-          `${defaultSize}${defaultUnit}`
-        );
+      expect(loader.find("span span").at(i)).toHaveStyleRule(
+        "height",
+        `${length}${unit || defaultUnit}`
+      );
+      expect(loader.find("span span").at(i)).toHaveStyleRule(
+        "width",
+        `${length}${unit || defaultUnit}`
+      );
+    }
+  };
+  lengthSpecs(PulseLoader, "size", sizeExpectStatements);
 
-        expect(loader.find("span span").at(i)).toHaveStyleRule("height", `${size}${defaultUnit}`);
-        expect(loader.find("span span").at(i)).toHaveStyleRule("width", `${size}${defaultUnit}`);
-      }
-    });
-
-    it("should render the size as is when size is a string with valid css unit", () => {
-      const size = "18px";
-      const loader = mount(<PulseLoader size={size} />);
-
-      for (let i = 0; i < 3; i++) {
-        expect(loader.find("span span").at(i)).not.toHaveStyleRule(
-          "height",
-          `${defaultSize}${defaultUnit}`
-        );
-        expect(loader.find("span span").at(i)).not.toHaveStyleRule(
-          "width",
-          `${defaultSize}${defaultUnit}`
-        );
-
-        expect(loader.find("span span").at(i)).toHaveStyleRule("height", `${size}`);
-        expect(loader.find("span span").at(i)).toHaveStyleRule("width", `${size}`);
-      }
-    });
-
-    it("should render the size with default unit of px when the unit is incorrect", () => {
-      const length = 18;
-      const unit = "ad";
-      const size = `${length}${unit}`;
-      const loader = mount(<PulseLoader size={size} />);
-
-      for (let i = 0; i < 3; i++) {
-        expect(loader.find("span span").at(i)).not.toHaveStyleRule(
-          "height",
-          `${defaultSize}${defaultUnit}`
-        );
-        expect(loader.find("span span").at(i)).not.toHaveStyleRule(
-          "width",
-          `${defaultSize}${defaultUnit}`
-        );
-
-        expect(loader.find("span span").at(i)).toHaveStyleRule("height", `${length}${defaultUnit}`);
-        expect(loader.find("span span").at(i)).toHaveStyleRule("width", `${length}${defaultUnit}`);
-      }
-    });
-  });
-
-  describe("margin props", () => {
-    it("should render the margin with px unit when margin is a number", () => {
-      const margin = 18;
-      const loader = mount(<PulseLoader margin={margin} />);
-
-      for (let i = 0; i < 3; i++) {
-        expect(loader.find("span span").at(i)).not.toHaveStyleRule(
-          "margin",
-          `${defaultMargin}${defaultUnit}`
-        );
-        expect(loader.find("span span").at(i)).toHaveStyleRule("margin", `${margin}${defaultUnit}`);
-      }
-    });
-
-    it("should render the margin as is when margin is a string with valid css unit", () => {
-      const margin = "18px";
-      const loader = mount(<PulseLoader margin={margin} />);
-
-      for (let i = 0; i < 3; i++) {
-        expect(loader.find("span span").at(i)).not.toHaveStyleRule(
-          "margin",
-          `${defaultMargin}${defaultUnit}`
-        );
-        expect(loader.find("span span").at(i)).toHaveStyleRule("margin", `${margin}`);
-      }
-    });
-
-    it("should render the margin with default unit of px when the unit is incorrect", () => {
-      const length = 18;
-      const unit = "ad";
-      const margin = `${length}${unit}`;
-      const loader = mount(<PulseLoader margin={margin} />);
-
-      for (let i = 0; i < 3; i++) {
-        expect(loader.find("span span").at(i)).not.toHaveStyleRule(
-          "margin",
-          `${defaultMargin}${defaultUnit}`
-        );
-        expect(loader.find("span span").at(i)).toHaveStyleRule("margin", `${length}${defaultUnit}`);
-      }
-    });
-  });
+  const marginExpectStatements = (loader: ReactWrapper, length: number, unit?: string) => {
+    for (let i = 0; i < 3; i++) {
+      expect(loader.find("span span").at(i)).not.toHaveStyleRule(
+        "margin",
+        `${defaultMargin}${defaultUnit}`
+      );
+      expect(loader.find("span span").at(i)).toHaveStyleRule(
+        "margin",
+        `${length}${unit || defaultUnit}`
+      );
+    }
+  };
+  lengthSpecs(PulseLoader, "margin", marginExpectStatements);
 
   const speedMultiplierExpectStatements = (loader: ReactWrapper, multiplier: number) => {
     for (let i = 0; i < 3; i++) {
@@ -163,11 +97,4 @@ describe("PulseLoader", () => {
     }
   };
   speedMultiplierSpecs(PulseLoader, speedMultiplierExpectStatements);
-
-  it("should render the css override based on props", () => {
-    const loader = mount(<PulseLoader css={"position: fixed; width: 100px; color: blue;"} />);
-    expect(loader).toHaveStyleRule("position", "fixed");
-    expect(loader).toHaveStyleRule("width", "100px");
-    expect(loader).toHaveStyleRule("color", "blue");
-  });
 });
