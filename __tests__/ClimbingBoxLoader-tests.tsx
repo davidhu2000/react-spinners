2fbc9f9edb40926e212155eb6a55c2cabf6f42c9
David Hu <davidhu314@gmail.com>
2021-03-13 20:12:39 -0800

    implement speed Multiplier for ClimbingBoxLoader

--- __tests__/ClimbingBoxLoader-tests.tsx
+++ __tests__/ClimbingBoxLoader-tests.tsx
@@ -5,12 +5,13 @@ expect.extend(matchers);
 
 import ClimbingBoxLoader from "../src/ClimbingBoxLoader";
 import { sizeDefaults } from "../src/helpers";
-import { commonSpecs, cssSpecs, lengthSpecs } from "./sharedSpecs";
+import { commonSpecs, cssSpecs, lengthSpecs, speedMultiplierSpecs } from "./sharedSpecs";
 
 describe("ClimbingBoxLoader", () => {
   const defaultColor = "#000000";
   const defaultSize = 15;
   const defaultUnit = "px";
+  const defaultSpeed = 2.5;
 
   commonSpecs(ClimbingBoxLoader, sizeDefaults(defaultSize));
   cssSpecs(ClimbingBoxLoader);
@@ -18,45 +19,32 @@ describe("ClimbingBoxLoader", () => {
   it("should contain styles created using default props", () => {
     const loader = mount(<ClimbingBoxLoader />);
     expect(loader.find("span span")).toHaveStyleRule("font-size", `${defaultSize}${defaultUnit}`);
-    expect(loader.find("span span span").at(0)).toHaveStyleRule(
-      "border",
-      `0.25em solid ${defaultColor}`
-    );
-    expect(loader.find("span span span").at(1)).toHaveStyleRule(
-      "border-left",
-      `0.25em solid ${defaultColor}`
-    );
+    expect(loader.find("span span span").at(0)).toHaveStyleRule("border", `0.25em solid ${defaultColor}`);
+    expect(loader.find("span span span").at(1)).toHaveStyleRule("border-left", `0.25em solid ${defaultColor}`);
   });
 
   it("should render the correct color based on prop", () => {
     const color = "#e2e2e2";
     const loader = mount(<ClimbingBoxLoader color={color} />);
 
-    expect(loader.find("span span span").at(0)).not.toHaveStyleRule(
-      "border",
-      `0.25em solid ${defaultColor}`
-    );
+    expect(loader.find("span span span").at(0)).not.toHaveStyleRule("border", `0.25em solid ${defaultColor}`);
     expect(loader.find("span span span").at(0)).toHaveStyleRule("border", `0.25em solid ${color}`);
 
-    expect(loader.find("span span span").at(1)).not.toHaveStyleRule(
-      "border-left",
-      `0.25em solid ${defaultColor}`
-    );
-    expect(loader.find("span span span").at(1)).toHaveStyleRule(
-      "border-left",
-      `0.25em solid ${color}`
-    );
+    expect(loader.find("span span span").at(1)).not.toHaveStyleRule("border-left", `0.25em solid ${defaultColor}`);
+    expect(loader.find("span span span").at(1)).toHaveStyleRule("border-left", `0.25em solid ${color}`);
   });
 
   const sizeExpectStatements = (loader: ReactWrapper, length: number, unit?: string) => {
-    expect(loader.find("span span")).not.toHaveStyleRule(
-      "font-size",
-      `${defaultSize}${defaultUnit}`
-    );
-    expect(loader.find("span span")).toHaveStyleRule(
-      "font-size",
-      `${length}${unit || defaultUnit}`
-    );
+    expect(loader.find("span span")).not.toHaveStyleRule("font-size", `${defaultSize}${defaultUnit}`);
+    expect(loader.find("span span")).toHaveStyleRule("font-size", `${length}${unit || defaultUnit}`);
   };
   lengthSpecs(ClimbingBoxLoader, "size", sizeExpectStatements);
+
+  const speedMultiplierExpectStatements = (loader: ReactWrapper, multiplier: number) => {
+    expect(loader.find("span span span").at(0)).toHaveStyleRule(
+      "animation",
+      expect.stringContaining(`${defaultSpeed * multiplier}s`)
+    );
+  };
+  speedMultiplierSpecs(ClimbingBoxLoader, speedMultiplierExpectStatements);
 });
