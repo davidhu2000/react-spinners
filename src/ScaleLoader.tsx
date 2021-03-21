b191754542d57fa2ebdfa31fb13656893d6a6d09
David Hu <davidhu314@gmail.com>
2021-03-21 13:35:00 -0700

    scale loader

--- src/ScaleLoader.tsx
+++ src/ScaleLoader.tsx
@@ -15,7 +15,7 @@ class Loader extends React.PureComponent<Required<LoaderHeightWidthRadiusProps>>
   public static defaultProps = heightWidthRadiusDefaults(35, 4, 2);
 
   public style = (i: number): SerializedStyles => {
-    const { color, width, height, margin, radius } = this.props;
+    const { color, width, height, margin, radius, speedMultiplier } = this.props;
 
     return css`
       background-color: ${color};
@@ -24,7 +24,7 @@ class Loader extends React.PureComponent<Required<LoaderHeightWidthRadiusProps>>
       margin: ${cssValue(margin)};
       border-radius: ${cssValue(radius)};
       display: inline-block;
-      animation: ${scale} 1s ${i * 0.1}s infinite cubic-bezier(0.2, 0.68, 0.18, 1.08);
+      animation: ${scale} ${1 / speedMultiplier}s ${i * 0.1}s infinite cubic-bezier(0.2, 0.68, 0.18, 1.08);
       animation-fill-mode: both;
     `;
   };
