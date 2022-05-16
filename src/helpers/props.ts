92299b3215427f5b8f55562bfe2ddaf3b2f7ba6d
David Hu <davidhu314@gmail.com>
2022-05-15 21:42:44 -0700

    remove

--- src/helpers/props.ts
+++ src/helpers/props.ts
@@ -13,13 +13,3 @@ export interface LoaderHeightWidthProps extends CommonProps {
   height?: LengthType;
   width?: LengthType;
 }
-
-/*
- * DefaultProps object for different loaders
- */
-export const commonProps = {
-  loading: true,
-  color: "#000000",
-  css: {},
-  speedMultiplier: 1
-};
