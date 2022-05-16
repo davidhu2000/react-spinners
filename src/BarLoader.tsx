4209dfaf4124c1f2b4632150410ba102b0c011a7
David Hu <davidhu314@gmail.com>
2022-05-16 01:04:11 -0400

    BarLoader: convert to function component and remove emotion (#473)

--- src/BarLoader.tsx
+++ src/BarLoader.tsx
@@ -1,66 +1,70 @@
-/** @jsxImportSource @emotion/react */
 import * as React from "react";
-import { keyframes, css, SerializedStyles } from "@emotion/react";
 
-import { calculateRgba, heightWidthDefaults, cssValue } from "./helpers";
-import { LoaderHeightWidthProps } from "./interfaces";
+import { calculateRgba, cssValue } from "./helpers";
+import { createAnimation } from "./helpers/animation";
+import { LoaderHeightWidthProps } from "./helpers/props";
 
-const long = keyframes`
-  0% {left: -35%;right: 100%}
-  60% {left: 100%;right: -90%}
-  100% {left: 100%;right: -90%}
-`;
+const long = createAnimation(
+  "BarLoader",
+  `0% {left: -35%;right: 100%} 60% {left: 100%;right: -90%} 100% {left: 100%;right: -90%}`,
+  "long"
+);
 
-const short = keyframes`
-  0% {left: -200%;right: 100%}
-  60% {left: 107%;right: -8%}
-  100% {left: 107%;right: -8%}
-`;
+const short = createAnimation(
+  "BarLoader",
+  `0% {left: -200%;right: 100%} 60% {left: 107%;right: -8%} 100% {left: 107%;right: -8%}`,
+  "short"
+);
 
-class Loader extends React.PureComponent<Required<LoaderHeightWidthProps>> {
-  public static defaultProps = heightWidthDefaults(4, 100);
-
-  public style = (i: number): SerializedStyles => {
-    const { height, color, speedMultiplier } = this.props;
-
-    return css`
-      position: absolute;
-      height: ${cssValue(height)};
-      overflow: hidden;
-      background-color: ${color};
-      background-clip: padding-box;
-      display: block;
-      border-radius: 2px;
-      will-change: left, right;
-      animation-fill-mode: forwards;
-      animation: ${i === 1 ? long : short} ${2.1 / speedMultiplier}s ${i === 2 ? `${1.15 / speedMultiplier}s` : ""}
-        ${i === 1 ? "cubic-bezier(0.65, 0.815, 0.735, 0.395)" : "cubic-bezier(0.165, 0.84, 0.44, 1)"} infinite;
-    `;
+function BarLoader({
+  loading = true,
+  color = "#000000",
+  speedMultiplier = 1,
+  css = {},
+  height = 4,
+  width = 100
+}: LoaderHeightWidthProps): JSX.Element | null {
+  const wrapper = (): React.CSSProperties => {
+    return Object.assign(
+      {
+        position: "relative",
+        width: cssValue(width),
+        height: cssValue(height),
+        overflow: "hidden",
+        backgroundColor: calculateRgba(color, 0.2),
+        backgroundClip: "padding-box"
+      },
+      css
+    );
   };
 
-  public wrapper = (): SerializedStyles => {
-    const { width, height, color } = this.props;
-
-    return css`
-      position: relative;
-      width: ${cssValue(width)};
-      height: ${cssValue(height)};
-      overflow: hidden;
-      background-color: ${calculateRgba(color, 0.2)};
-      background-clip: padding-box;
-    `;
+  const style = (i: number): React.CSSProperties => {
+    return {
+      position: "absolute",
+      height: cssValue(height),
+      overflow: "hidden",
+      backgroundColor: color,
+      backgroundClip: "padding-box",
+      display: "block",
+      borderRadius: 2,
+      willChange: "left, right",
+      animationFillMode: "forwards",
+      animation: `${i === 1 ? long : short} ${2.1 / speedMultiplier}s ${i === 2 ? `${1.15 / speedMultiplier}s` : ""} ${
+        i === 1 ? "cubic-bezier(0.65, 0.815, 0.735, 0.395)" : "cubic-bezier(0.165, 0.84, 0.44, 1)"
+      } infinite`
+    };
   };
 
-  public render(): JSX.Element | null {
-    const { loading, css } = this.props;
-
-    return loading ? (
-      <span css={[this.wrapper(), css]}>
-        <span css={this.style(1)} />
-        <span css={this.style(2)} />
-      </span>
-    ) : null;
+  if (!loading) {
+    return null;
   }
+
+  return (
+    <span style={wrapper()}>
+      <span style={style(1)} />
+      <span style={style(2)} />
+    </span>
+  );
 }
 
-export default Loader;
+export default BarLoader;
