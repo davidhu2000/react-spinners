c748db5cd1f05d60ee3a03d72604a671affc0686
David Hu <davidhu314@gmail.com>
2022-05-20 21:46:34 -0700

    CircleLoader convert to fucntional (#480)
    * CircleLoader convert to fucntional
    
    * update
    
    * fix

--- src/CircleLoader.tsx
+++ src/CircleLoader.tsx
@@ -1,62 +1,61 @@
-/** @jsxImportSource @emotion/react */
 import * as React from "react";
-import { keyframes, css, SerializedStyles } from "@emotion/react";
 
-import { sizeDefaults, cssValue, parseLengthAndUnit } from "./helpers";
-import { LoaderSizeProps } from "./interfaces";
+import { cssValue, parseLengthAndUnit } from "./helpers";
+import { LoaderSizeProps } from "./helpers/props";
+import { createAnimation } from "./helpers/animation";
 
-const circle = keyframes`
-  0% {transform: rotate(0deg)}
-  50% {transform: rotate(180deg)}
-  100% {transform: rotate(360deg)}
-`;
+const circle = createAnimation(
+  "CircleLoader",
+  "0% {transform: rotate(0deg)} 50% {transform: rotate(180deg)} 100% {transform: rotate(360deg)}"
+);
 
-class Loader extends React.PureComponent<Required<LoaderSizeProps>> {
-  public static defaultProps = sizeDefaults(50);
+function CircleLoader({
+  loading = true,
+  color = "#000000",
+  speedMultiplier = 1,
+  css = {},
+  size = 4,
+  ...additionalprops
+}: LoaderSizeProps): JSX.Element | null {
+  const wrapper: React.CSSProperties = {
+    position: "relative",
+    width: cssValue(size),
+    height: cssValue(size),
+    ...css,
+  };
 
-  public style = (i: number): SerializedStyles => {
-    const { size, color, speedMultiplier } = this.props;
+  const style = (i: number): React.CSSProperties => {
     const { value, unit } = parseLengthAndUnit(size);
 
-    return css`
-      position: absolute;
-      height: ${`${value * (1 - i / 10)}${unit}`};
-      width: ${`${value * (1 - i / 10)}${unit}`};
-      border: 1px solid ${color};
-      border-radius: 100%;
-      transition: 2s;
-      border-bottom: none;
-      border-right: none;
-      top: ${i * 0.7 * 2.5}%;
-      left: ${i * 0.35 * 2.5}%;
-      animation-fill-mode: "";
-      animation: ${circle} ${1 / speedMultiplier}s ${(i * 0.2) / speedMultiplier}s infinite linear;
-    `;
+    return {
+      position: "absolute",
+      height: `${value * (1 - i / 10)}${unit}`,
+      width: `${value * (1 - i / 10)}${unit}`,
+      border: `1px solid ${color}`,
+      borderRadius: "100%",
+      transition: "2s",
+      borderBottom: "none",
+      borderRight: "none",
+      top: `${i * 0.7 * 2.5}%`,
+      left: `${i * 0.35 * 2.5}%`,
+      animationFillMode: "",
+      animation: `${circle} ${1 / speedMultiplier}s ${(i * 0.2) / speedMultiplier}s infinite linear`,
+    };
   };
 
-  public wrapper = (): SerializedStyles => {
-    const { size } = this.props;
-
-    return css`
-      position: relative;
-      width: ${cssValue(size)};
-      height: ${cssValue(size)};
-    `;
-  };
-
-  public render(): JSX.Element | null {
-    const { loading, css } = this.props;
-
-    return loading ? (
-      <span css={[this.wrapper(), css]}>
-        <span css={this.style(0)} />
-        <span css={this.style(1)} />
-        <span css={this.style(2)} />
-        <span css={this.style(3)} />
-        <span css={this.style(4)} />
-      </span>
-    ) : null;
+  if (!loading) {
+    return null;
   }
+
+  return (
+    <span style={wrapper} {...additionalprops}>
+      <span style={style(0)} />
+      <span style={style(1)} />
+      <span style={style(2)} />
+      <span style={style(3)} />
+      <span style={style(4)} />
+    </span>
+  );
 }
 
-export default Loader;
+export default CircleLoader;
