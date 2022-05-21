a3d827749bec43da3289f73d672df4f856b1d002
David Hu <davidhu314@gmail.com>
2022-05-21 12:32:56 -0700

    ClockLoader migrate to function

--- src/ClockLoader.tsx
+++ src/ClockLoader.tsx
@@ -1,62 +1,64 @@
-/** @jsxImportSource @emotion/react */
 import * as React from "react";
-import { keyframes, css, SerializedStyles } from "@emotion/react";
 
-import { sizeDefaults, parseLengthAndUnit } from "./helpers";
-import { LoaderSizeProps } from "./interfaces";
+import { parseLengthAndUnit } from "./helpers";
+import { LoaderSizeProps } from "./helpers/props";
+import { createAnimation } from "./helpers/animation";
 
-const rotate = keyframes`
+const rotate = createAnimation(
+  "ClockLoader",
+  `
   100% { transform: rotate(360deg) }
-`;
+`
+);
 
-class Loader extends React.PureComponent<Required<LoaderSizeProps>> {
-  public static defaultProps = sizeDefaults(50);
+function ClockLoader({
+  loading = true,
+  color = "#000000",
+  speedMultiplier = 1,
+  css = {},
+  size = 50,
+  ...additionalprops
+}: LoaderSizeProps): JSX.Element | null {
+  const { value, unit } = parseLengthAndUnit(size);
 
-  public wrapper = (): SerializedStyles => {
-    const { size, color, speedMultiplier } = this.props;
+  const wrapper: React.CSSProperties = {
+    position: `relative`,
+    width: `${value}${unit}`,
+    height: `${value}${unit}`,
+    backgroundColor: `transparent`,
+    boxShadow: `inset 0px 0px 0px 2px ${color}`,
+    borderRadius: `50%`,
 
-    const { value, unit } = parseLengthAndUnit(size);
-
-    return css`
-      position: relative;
-      width: ${`${value}${unit}`};
-      height: ${`${value}${unit}`};
-      background-color: transparent;
-      box-shadow: inset 0px 0px 0px 2px ${color};
-      border-radius: 50%;
-
-      &:after,
-      &:before {
-        position: absolute;
-        content: "";
-        background-color: ${color};
-      }
-
-      &:after {
-        width: ${value / 2.4}px;
-        height: 2px;
-        top: ${value / 2 - 1}px;
-        left: ${value / 2 - 1}px;
-        transform-origin: 1px 1px;
-        animation: ${rotate} ${2 / speedMultiplier}s linear infinite;
-      }
-
-      &:before {
-        width: ${value / 3}px;
-        height: 2px;
-        top: ${value / 2 - 1}px;
-        left: ${value / 2 - 1}px;
-        transform-origin: 1px 1px;
-        animation: ${rotate} ${8 / speedMultiplier}s linear infinite;
-      }
-    `;
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore the keys are valid css classes
+    "&:before,&:after": {
+      position: "absolute",
+      content: "",
+      backgroundColor: color,
+    },
+    "&:after": {
+      width: `${value / 2.4}px`,
+      height: `2px`,
+      top: `${value / 2 - 1}px`,
+      left: `${value / 2 - 1}px`,
+      transformOrigin: `1px 1px`,
+      animation: `${rotate} ${2 / speedMultiplier}s linear infinite`,
+    },
+    "&:before": {
+      width: `${value / 3}px`,
+      height: `2px`,
+      top: `${value / 2 - 1}px`,
+      left: `${value / 2 - 1}px`,
+      transformOrigin: `1px 1px`,
+      animation: `${rotate} ${8 / speedMultiplier}s linear infinite`,
+    },
   };
 
-  public render(): JSX.Element | null {
-    const { loading, css } = this.props;
-
-    return loading ? <span css={[this.wrapper(), css]} /> : null;
+  if (!loading) {
+    return null;
   }
+
+  return <span style={wrapper} {...additionalprops} />;
 }
 
-export default Loader;
+export default ClockLoader;
