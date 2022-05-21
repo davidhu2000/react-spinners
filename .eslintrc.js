008584244e43e0a34267a611830e05a44962bd79
David Hu <davidhu314@gmail.com>
2022-05-20 21:24:24 -0700

    add unicorn, testing-library, and js-dom eslint plugins (#479)
    * add react testing library eslint plugins
    
    * eslints
    
    * fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "@emotion"],
+  plugins: ["@typescript-eslint", "@emotion", "unicorn", "jest-dom", "testing-library"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react/recommended", "prettier"],
   settings: {
     react: {
-      version: "detect"
-    }
-  }
+      version: "detect",
+    },
+  },
 };
