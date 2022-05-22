1822a39411edad0ddbc66b8c8b4358445294cbe4
David Hu <davidhu314@gmail.com>
2022-05-22 12:21:54 -0700

    Square loader convert to functional (#499)
    * SquareLoader convert to functional
    
    * format
    
    * format

--- src/SquareLoader.tsx
+++ src/SquareLoader.tsx
@@ -1,38 +1,41 @@
-/** @jsxImportSource @emotion/react */
 import * as React from "react";
-import { keyframes, css, SerializedStyles } from "@emotion/react";
 
-import { sizeDefaults, cssValue } from "./helpers";
-import { LoaderSizeProps } from "./interfaces";
+import { cssValue } from "./helpers";
+import { LoaderSizeProps } from "./helpers/props";
+import { createAnimation } from "./helpers/animation";
 
-const square = keyframes`
-  25% {transform: rotateX(180deg) rotateY(0)}
-  50% {transform: rotateX(180deg) rotateY(180deg)}
-  75% {transform: rotateX(0) rotateY(180deg)}
-  100% {transform: rotateX(0) rotateY(0)}
-`;
+const square = createAnimation(
+  "SquareLoader",
+  `25% {transform: rotateX(180deg) rotateY(0)}
+  50% {transform: rotateX(180deg) rotateY(180deg)} 
+  75% {transform: rotateX(0) rotateY(180deg)} 
+  100% {transform: rotateX(0) rotateY(0)}`,
+  "square"
+);
 
-class Loader extends React.PureComponent<Required<LoaderSizeProps>> {
-  public static defaultProps = sizeDefaults(50);
-
-  public style = (): SerializedStyles => {
-    const { color, size, speedMultiplier } = this.props;
-
-    return css`
-      background-color: ${color};
-      width: ${cssValue(size)};
-      height: ${cssValue(size)};
-      display: inline-block;
-      animation: ${square} ${3 / speedMultiplier}s 0s infinite cubic-bezier(0.09, 0.57, 0.49, 0.9);
-      animation-fill-mode: both;
-    `;
+function SquareLoader({
+  loading = true,
+  color = "#000000",
+  speedMultiplier = 1,
+  css = {},
+  size = 50,
+  ...additionalprops
+}: LoaderSizeProps): JSX.Element | null {
+  const style: React.CSSProperties = {
+    backgroundColor: color,
+    width: cssValue(size),
+    height: cssValue(size),
+    display: "inline-block",
+    animation: `${square} ${3 / speedMultiplier}s 0s infinite cubic-bezier(0.09, 0.57, 0.49, 0.9)`,
+    animationFillMode: "both",
+    ...css,
   };
 
-  public render(): JSX.Element | null {
-    const { loading, css } = this.props;
-
-    return loading ? <span css={[this.style(), css]} /> : null;
+  if (!loading) {
+    return null;
   }
+
+  return <span style={style} {...additionalprops} />;
 }
 
-export default Loader;
+export default SquareLoader;
