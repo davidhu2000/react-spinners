1d53a462de750b23c68ca685d4fbdd3d34849f8d
David Hu <davidhu314@gmail.com>
2022-05-22 13:10:34 -0700

    remove

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "@emotion", "unicorn", "jest-dom", "testing-library"],
+  plugins: ["@typescript-eslint", "unicorn", "jest-dom", "testing-library"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react/recommended", "prettier"],
   settings: {
     react: {
