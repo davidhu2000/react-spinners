06fd5945548de9e70d9d1120828c53dcde363e94
David Hu <davidhu314@gmail.com>
2022-05-22 11:42:05 -0700

    RingLoader convert to functional (#493)
    * RingLoader convert to functional
    
    * lint

--- src/RingLoader.tsx
+++ src/RingLoader.tsx
@@ -1,64 +1,68 @@
-/** @jsxImportSource @emotion/react */
 import * as React from "react";
-import { keyframes, css, SerializedStyles } from "@emotion/react";
 
-import { sizeDefaults, parseLengthAndUnit, cssValue } from "./helpers";
-import { LoaderSizeProps, LengthType } from "./interfaces";
+import { parseLengthAndUnit, cssValue } from "./helpers";
+import { LoaderSizeProps } from "./helpers/props";
+import { createAnimation } from "./helpers/animation";
 
-const right = keyframes`
+const right = createAnimation(
+  "RingLoader",
+  `
   0% {transform: rotateX(0deg) rotateY(0deg) rotateZ(0deg)}
   100% {transform: rotateX(180deg) rotateY(360deg) rotateZ(360deg)}
-`;
+`
+);
 
-const left = keyframes`
+const left = createAnimation(
+  "RingLoader",
+  `
   0% {transform: rotateX(0deg) rotateY(0deg) rotateZ(0deg)}
   100% {transform: rotateX(360deg) rotateY(180deg) rotateZ(360deg)}
-`;
+`
+);
 
-class Loader extends React.PureComponent<Required<LoaderSizeProps>> {
-  public static defaultProps = sizeDefaults(60);
+function RingLoader({
+  loading = true,
+  color = "#000000",
+  speedMultiplier = 1,
+  css = {},
+  size = 60,
+  ...additionalprops
+}: LoaderSizeProps): JSX.Element | null {
+  const { value, unit } = parseLengthAndUnit(size);
 
-  public getSize = (): LengthType => {
-    return this.props.size;
+  const style = (i: number): React.CSSProperties => {
+    return {
+      position: "absolute",
+      top: "0",
+      left: "0",
+      width: `${value}${unit}`,
+      height: `${value}${unit}`,
+      border: `${value / 10}${unit} solid ${color}`,
+      opacity: "0.4",
+      borderRadius: "100%",
+      animationFillMode: "forwards",
+      perspective: "800px",
+      animation: `${i === 1 ? right : left} ${2 / speedMultiplier}s 0s infinite linear`,
+    };
   };
 
-  public style = (i: number): SerializedStyles => {
-    const { color, speedMultiplier } = this.props;
-    const { value, unit } = parseLengthAndUnit(this.getSize());
-
-    return css`
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: ${`${value}${unit}`};
-      height: ${`${value}${unit}`};
-      border: ${`${value / 10}${unit}`} solid ${color};
-      opacity: 0.4;
-      border-radius: 100%;
-      animation-fill-mode: forwards;
-      perspective: 800px;
-      animation: ${i === 1 ? right : left} ${2 / speedMultiplier}s 0s infinite linear;
-    `;
+  const wrapper: React.CSSProperties = {
+    width: cssValue(size),
+    height: cssValue(size),
+    position: "relative",
+    ...css,
   };
 
-  public wrapper = (): SerializedStyles => {
-    return css`
-      width: ${cssValue(this.getSize())};
-      height: ${cssValue(this.getSize())};
-      position: relative;
-    `;
-  };
-
-  public render(): JSX.Element | null {
-    const { loading, css } = this.props;
-
-    return loading ? (
-      <span css={[this.wrapper(), css]}>
-        <span css={this.style(1)} />
-        <span css={this.style(2)} />
-      </span>
-    ) : null;
+  if (!loading) {
+    return null;
   }
+
+  return (
+    <span style={wrapper} {...additionalprops}>
+      <span style={style(1)} />
+      <span style={style(2)} />
+    </span>
+  );
 }
 
-export default Loader;
+export default RingLoader;
