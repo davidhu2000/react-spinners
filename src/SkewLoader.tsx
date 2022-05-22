38f1b15b69479ebe67421bfe768da3ef985cec83
David Hu <davidhu314@gmail.com>
2022-05-22 12:13:00 -0700

    SkewLoader convert to functional (#498)

--- src/SkewLoader.tsx
+++ src/SkewLoader.tsx
@@ -1,40 +1,40 @@
-/** @jsxImportSource @emotion/react */
 import * as React from "react";
-import { keyframes, css, SerializedStyles } from "@emotion/react";
 
-import { sizeDefaults, cssValue } from "./helpers";
-import { LoaderSizeProps } from "./interfaces";
+import { cssValue } from "./helpers";
+import { LoaderSizeProps } from "./helpers/props";
+import { createAnimation } from "./helpers/animation";
 
-const skew = keyframes`
-  25% {transform: perspective(100px) rotateX(180deg) rotateY(0)}
-  50% {transform: perspective(100px) rotateX(180deg) rotateY(180deg)}
-  75% {transform: perspective(100px) rotateX(0) rotateY(180deg)}
-  100% {transform: perspective(100px) rotateX(0) rotateY(0)}
-`;
+const skew = createAnimation(
+  "SkewLoader",
+  "25% {transform: perspective(100px) rotateX(180deg) rotateY(0)} 50% {transform: perspective(100px) rotateX(180deg) rotateY(180deg)} 75% {transform: perspective(100px) rotateX(0) rotateY(180deg)} 100% {transform: perspective(100px) rotateX(0) rotateY(0)}",
+  "skew"
+);
 
-class Loader extends React.PureComponent<Required<LoaderSizeProps>> {
-  public static defaultProps = sizeDefaults(20);
-
-  public style = (): SerializedStyles => {
-    const { color, speedMultiplier, size } = this.props;
-
-    return css`
-      width: 0;
-      height: 0;
-      border-left: ${cssValue(size)} solid transparent;
-      border-right: ${cssValue(size)} solid transparent;
-      border-bottom: ${cssValue(size)} solid ${color};
-      display: inline-block;
-      animation: ${skew} ${3 / speedMultiplier}s 0s infinite cubic-bezier(0.09, 0.57, 0.49, 0.9);
-      animation-fill-mode: both;
-    `;
+function SkewLoader({
+  loading = true,
+  color = "#000000",
+  speedMultiplier = 1,
+  css = {},
+  size = 20,
+  ...additionalprops
+}: LoaderSizeProps): JSX.Element | null {
+  const style: React.CSSProperties = {
+    width: "0",
+    height: "0",
+    borderLeft: `${cssValue(size)} solid transparent`,
+    borderRight: `${cssValue(size)} solid transparent`,
+    borderBottom: `${cssValue(size)} solid ${color}`,
+    display: "inline-block",
+    animation: `${skew} ${3 / speedMultiplier}s 0s infinite cubic-bezier(0.09, 0.57, 0.49, 0.9)`,
+    animationFillMode: "both",
+    ...css,
   };
 
-  public render(): JSX.Element | null {
-    const { loading, css } = this.props;
-
-    return loading ? <span css={[this.style(), css]} /> : null;
+  if (!loading) {
+    return null;
   }
+
+  return <span style={style} {...additionalprops} />;
 }
 
-export default Loader;
+export default SkewLoader;
