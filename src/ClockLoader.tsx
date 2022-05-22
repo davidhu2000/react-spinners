b2706ceaf30133c3588ee1f7709fea92a3c95d3b
David Hu <davidhu314@gmail.com>
2022-05-22 12:34:30 -0700

    fix

--- src/ClockLoader.tsx
+++ src/ClockLoader.tsx
@@ -23,38 +23,41 @@ function ClockLoader({
     backgroundColor: "transparent",
     boxShadow: `inset 0px 0px 0px 2px ${color}`,
     borderRadius: "50%",
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore the keys are valid css classes
-    "&:before,&:after": {
-      position: "absolute",
-      content: "",
-      backgroundColor: color,
-    },
-    "&:after": {
-      width: `${value / 2.4}px`,
-      height: "2px",
-      top: `${value / 2 - 1}px`,
-      left: `${value / 2 - 1}px`,
-      transformOrigin: "1px 1px",
-      animation: `${rotate} ${2 / speedMultiplier}s linear infinite`,
-    },
-    "&:before": {
-      width: `${value / 3}px`,
-      height: "2px",
-      top: `${value / 2 - 1}px`,
-      left: `${value / 2 - 1}px`,
-      transformOrigin: "1px 1px",
-      animation: `${rotate} ${8 / speedMultiplier}s linear infinite`,
-    },
     ...css,
+  };
+
+  const minute: React.CSSProperties = {
+    position: "absolute",
+    backgroundColor: color,
+    width: `${value / 3}px`,
+    height: "2px",
+    top: `${value / 2 - 1}px`,
+    left: `${value / 2 - 1}px`,
+    transformOrigin: "1px 1px",
+    animation: `${rotate} ${8 / speedMultiplier}s linear infinite`,
+  };
+
+  const hour: React.CSSProperties = {
+    position: "absolute",
+    backgroundColor: color,
+    width: `${value / 2.4}px`,
+    height: "2px",
+    top: `${value / 2 - 1}px`,
+    left: `${value / 2 - 1}px`,
+    transformOrigin: "1px 1px",
+    animation: `${rotate} ${2 / speedMultiplier}s linear infinite`,
   };
 
   if (!loading) {
     return null;
   }
 
-  return <span style={wrapper} {...additionalprops} />;
+  return (
+    <span style={wrapper} {...additionalprops}>
+      <span style={hour} />
+      <span style={minute} />
+    </span>
+  );
 }
 
 export default ClockLoader;
