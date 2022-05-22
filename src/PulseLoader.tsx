d0b0a04751e64165344b164f2be7e8215895847b
David Hu <davidhu314@gmail.com>
2022-05-22 11:35:23 -0700

    PulseLoader convert to functional (#492)

--- src/PulseLoader.tsx
+++ src/PulseLoader.tsx
@@ -1,46 +1,53 @@
-/** @jsxImportSource @emotion/react */
 import * as React from "react";
-import { keyframes, css, SerializedStyles } from "@emotion/react";
 
-import { sizeMarginDefaults, cssValue } from "./helpers";
-import { LoaderSizeMarginProps } from "./interfaces";
+import { cssValue } from "./helpers";
+import { LoaderSizeMarginProps } from "./helpers/props";
+import { createAnimation } from "./helpers/animation";
 
-const pulse = keyframes`
-  0% {transform: scale(1);opacity: 1}
-  45% {transform: scale(0.1);opacity: 0.7}
-  80% {transform: scale(1);opacity: 1}
-`;
+const pulse = createAnimation(
+  "PulseLoader",
+  `
+  0% {transform: scale(1),opacity: 1}
+  45% {transform: scale(0.1),opacity: 0.7}
+  80% {transform: scale(1),opacity: 1}
+`
+);
 
-class Loader extends React.PureComponent<Required<LoaderSizeMarginProps>> {
-  public static defaultProps = sizeMarginDefaults(15);
-
-  public style = (i: number): SerializedStyles => {
-    const { color, size, margin, speedMultiplier } = this.props;
-
-    return css`
-      background-color: ${color};
-      width: ${cssValue(size)};
-      height: ${cssValue(size)};
-      margin: ${cssValue(margin)};
-      border-radius: 100%;
-      display: inline-block;
-      animation: ${pulse} ${0.75 / speedMultiplier}s ${(i * 0.12) / speedMultiplier}s infinite
-        cubic-bezier(0.2, 0.68, 0.18, 1.08);
-      animation-fill-mode: both;
-    `;
+function PulseLoader({
+  loading = true,
+  color = "#000000",
+  speedMultiplier = 1,
+  css = {},
+  size = 15,
+  margin = 2,
+  ...additionalprops
+}: LoaderSizeMarginProps): JSX.Element | null {
+  const style = (i: number): React.CSSProperties => {
+    return {
+      backgroundColor: color,
+      width: cssValue(size),
+      height: cssValue(size),
+      margin: cssValue(margin),
+      borderRadius: "100%",
+      display: "inline-block",
+      animation: `${pulse} ${0.75 / speedMultiplier}s ${
+        (i * 0.12) / speedMultiplier
+      }s infinite cubic-bezier(0.2, 0.68, 0.18, 1.08)`,
+      animationFillMode: "both",
+    };
   };
 
-  public render(): JSX.Element | null {
-    const { loading, css } = this.props;
-
-    return loading ? (
-      <span css={[css]}>
-        <span css={this.style(1)} />
-        <span css={this.style(2)} />
-        <span css={this.style(3)} />
-      </span>
-    ) : null;
+  if (!loading) {
+    return null;
   }
+
+  return (
+    <span style={css} {...additionalprops}>
+      <span style={style(1)} />
+      <span style={style(2)} />
+      <span style={style(3)} />
+    </span>
+  );
 }
 
-export default Loader;
+export default PulseLoader;
