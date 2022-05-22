0ac06327d584526873ad83751003c69c6ff10bb8
David Hu <davidhu314@gmail.com>
2022-05-22 12:50:57 -0700

    fix

--- jest.config.js
+++ jest.config.js
@@ -3,16 +3,11 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: "./coverage",
   moduleDirectories: ["node_modules"],
-  testPathIgnorePatterns: [
-    "<rootDir>/__tests__/config/*",
-    "<rootDir>/__tests__/mock/*",
-    "<rootDir>/__tests__/sharedSpecs/*",
-  ],
   transform: {
     ".(ts|tsx|js|jsx)": "ts-jest",
   },
   roots: ["<rootDir>"],
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(t|j)sx?$",
+  testRegex: "(/__tests__/.*|(\\.|/)(tests|spec))\\.(t|j)sx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   snapshotSerializers: ["@emotion/jest/serializer"],
   testEnvironment: "jsdom",
