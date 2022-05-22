740f030558277b1cf02d3365aaad1fb116b54535
David Hu <davidhu314@gmail.com>
2022-05-22 11:24:34 -0700

    PropagateLoader onvert to functional (#490)

--- src/PropagateLoader.tsx
+++ src/PropagateLoader.tsx
@@ -1,89 +1,109 @@
-/** @jsxImportSource @emotion/react */
 import * as React from "react";
-import { keyframes, css, SerializedStyles } from "@emotion/react";
 
-import { sizeDefaults, parseLengthAndUnit } from "./helpers";
-import { LoaderSizeProps } from "./interfaces";
+import { parseLengthAndUnit } from "./helpers";
+import { LoaderSizeProps } from "./helpers/props";
+import { createAnimation } from "./helpers/animation";
 
 // 1.5 4.5 7.5
 const distance = [1, 3, 5];
 
 const propagate = [
-  keyframes`
+  createAnimation(
+    "PropagateLoader",
+    `
       25% {transform: translateX(-${distance[0]}rem) scale(0.75)}
       50% {transform: translateX(-${distance[1]}rem) scale(0.6)}
       75% {transform: translateX(-${distance[2]}rem) scale(0.5)}
       95% {transform: translateX(0rem) scale(1)}
-    `,
-  keyframes`
+    `
+  ),
+  createAnimation(
+    "PropagateLoader",
+    `
       25% {transform: translateX(-${distance[0]}rem) scale(0.75)}
       50% {transform: translateX(-${distance[1]}rem) scale(0.6)}
       75% {transform: translateX(-${distance[1]}rem) scale(0.6)}
       95% {transform: translateX(0rem) scale(1)}
-    `,
-  keyframes`
+    `
+  ),
+  createAnimation(
+    "PropagateLoader",
+    `
       25% {transform: translateX(-${distance[0]}rem) scale(0.75)}
       75% {transform: translateX(-${distance[0]}rem) scale(0.75)}
       95% {transform: translateX(0rem) scale(1)}
-    `,
-  keyframes`
+    `
+  ),
+  createAnimation(
+    "PropagateLoader",
+    `
       25% {transform: translateX(${distance[0]}rem) scale(0.75)}
       75% {transform: translateX(${distance[0]}rem) scale(0.75)}
       95% {transform: translateX(0rem) scale(1)}
-    `,
-  keyframes`
+    `
+  ),
+  createAnimation(
+    "PropagateLoader",
+    `
       25% {transform: translateX(${distance[0]}rem) scale(0.75)}
       50% {transform: translateX(${distance[1]}rem) scale(0.6)}
       75% {transform: translateX(${distance[1]}rem) scale(0.6)}
       95% {transform: translateX(0rem) scale(1)}
-    `,
-  keyframes`
+    `
+  ),
+  createAnimation(
+    "PropagateLoader",
+    `
       25% {transform: translateX(${distance[0]}rem) scale(0.75)}
       50% {transform: translateX(${distance[1]}rem) scale(0.6)}
       75% {transform: translateX(${distance[2]}rem) scale(0.5)}
       95% {transform: translateX(0rem) scale(1)}
     `
+  ),
 ];
 
-class Loader extends React.PureComponent<Required<LoaderSizeProps>> {
-  public static defaultProps = sizeDefaults(15);
+function PropagateLoader({
+  loading = true,
+  color = "#000000",
+  speedMultiplier = 1,
+  css = {},
+  size = 15,
+  ...additionalprops
+}: LoaderSizeProps): JSX.Element | null {
+  const { value, unit } = parseLengthAndUnit(size);
 
-  public style = (i: number): SerializedStyles => {
-    const { size, color, speedMultiplier } = this.props;
-    const { value, unit } = parseLengthAndUnit(size);
-
-    return css`
-      position: absolute;
-      font-size: ${`${value / 3}${unit}`};
-      width: ${`${value}${unit}`};
-      height: ${`${value}${unit}`};
-      background: ${color};
-      border-radius: 50%;
-      animation: ${propagate[i]} ${1.5 / speedMultiplier}s infinite;
-      animation-fill-mode: forwards;
-    `;
+  const style = (i: number): React.CSSProperties => {
+    return {
+      position: "absolute",
+      fontSize: `${value / 3}${unit}`,
+      width: `${value}${unit}`,
+      height: `${value}${unit}`,
+      background: color,
+      borderRadius: "50%",
+      animation: `${propagate[i]} ${1.5 / speedMultiplier}s infinite`,
+      animationFillMode: "forwards",
+    };
   };
 
-  public wrapper = (): SerializedStyles => {
-    return css`
-      position: relative;
-    `;
+  const wrapper: React.CSSProperties = {
+    position: "relative",
+    ...css,
   };
 
-  public render(): JSX.Element | null {
-    const { loading, css } = this.props;
-
-    return loading ? (
-      <span css={[this.wrapper(), css]}>
-        <span css={this.style(0)} />
-        <span css={this.style(1)} />
-        <span css={this.style(2)} />
-        <span css={this.style(3)} />
-        <span css={this.style(4)} />
-        <span css={this.style(5)} />
-      </span>
-    ) : null;
+  if (!loading) {
+    return null;
   }
+
+  return (
+    <span style={wrapper} {...additionalprops}>
+      <span style={style(0)} />
+      <span style={style(1)} />
+      <span style={style(2)} />
+      <span style={style(3)} />
+      <span style={style(4)} />
+      <span style={style(5)} />
+    </span>
+  );
 }
 
-export default Loader;
+export default PropagateLoader;
