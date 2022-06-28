7ed108bd0fb33ec6b29a72f9a106c234e69b13b8
David Hu <davidhu314@gmail.com>
2022-06-27 22:35:25 -0700

    add google analytics to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,11 @@
 module.exports = {
   stories: ["../stories/*.stories.@(ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-google-analytics",
+  ],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
