1b1da703a5c19d0c5e333fcdf4a77bdc4d1f10a4
David Hu <davidhu314@gmail.com>
2022-08-31 21:47:20 -0700

    update storybook to use google analytics v4 (#534)

---
+++ .storybook/google-analytics-v4/register.js
@@ -0,0 +1,27 @@
+import { window } from "global";
+import { addons } from "@storybook/addons";
+import { STORY_CHANGED, STORY_ERRORED, STORY_MISSING } from "@storybook/core-events";
+
+import ReactGa from "react-ga4";
+
+addons.register("storybook/google-analytics-v4", (api) => {
+  console.log("registering google analytics v4");
+  ReactGa.initialize(window.STORYBOOK_GA_V4_ID, window.STORYBOOK_REACT_GA_OPTIONS);
+
+  api.on(STORY_CHANGED, () => {
+    const { path } = api.getUrlState();
+    ReactGa.send({ hitType: "pageview", page: path });
+  });
+  api.on(STORY_ERRORED, ({ description }) => {
+    ReactGa.exception({
+      description,
+      fatal: true,
+    });
+  });
+  api.on(STORY_MISSING, (id) => {
+    ReactGa.exception({
+      description: `attempted to render ${id}, but it is missing`,
+      fatal: false,
+    });
+  });
+});
