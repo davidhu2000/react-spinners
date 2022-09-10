a77fe1e4390f72061434d1a41a9b64f4e92c19b4
David Hu <davidhu314@gmail.com>
2022-09-09 21:08:54 -0700

    move script into docs code instead of standpoint script

--- examples/index.tsx
+++ examples/index.tsx
@@ -74,3 +74,21 @@ document.addEventListener("DOMContentLoaded", () => {
     reactCode.render(<Code />);
   }
 });
+
+const header = document.getElementById("header") as HTMLElement;
+const main = document.getElementById("main") as HTMLElement;
+document.addEventListener("scroll", () => {
+  const height = 360 - window.scrollY;
+  if (height > 200) {
+    header.style.height = `${height}px`;
+    header.classList.remove("navbar");
+    header.style.boxShadow = "";
+    main.style.marginTop = "0";
+  } else {
+    header.classList.add("navbar");
+    header.style.height = "";
+    header.style.boxShadow =
+      "0 3px 3px 0 rgba(0,0,0,0.14), 0 1px 7px 0 rgba(0,0,0,0.12), 0 3px 1px -1px rgba(0,0,0,0.2)";
+    main.style.marginTop = "250px";
+  }
+});
