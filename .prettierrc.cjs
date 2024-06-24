0e42b7c5b097307d5c99cdabf6daa63408ba9c46
David Hu <davidhu314@gmail.com>
2024-06-24 09:26:53 -0700

    chore: upgrade prettier to v3

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Options} */
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  trailingComma: "es5",
+  singleQuote: false,
+  semi: true,
+  pluginSearchDirs: false,
+};
