4657c6b44d541264f764ecb023285801b3dce49d
David Hu <davidhu314@gmail.com>
2024-06-26 09:16:35 -0700

    Revert "fix: multiple hash loader with different color renders as the same co…"
    This reverts commit ae4a2c5b2ddb17fa7a88f3edb890f9509062ba17.

--- src/helpers/animation.server.test.ts
+++ src/helpers/animation.server.test.ts
@@ -11,6 +11,6 @@ describe("animation", () => {
       "0% {left: -35%;right: 100%} 60% {left: 100%;right: -90%} 100% {left: 100%;right: -90%}",
       "my-suffix"
     );
-    expect(name).toMatch(/^react-spinners-TestLoader-my-suffix/);
+    expect(name).toEqual("react-spinners-TestLoader-my-suffix");
   });
 });
