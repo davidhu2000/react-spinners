ae4a2c5b2ddb17fa7a88f3edb890f9509062ba17
David Hu <davidhu314@gmail.com>
2024-06-25 22:01:47 -0700

    fix: multiple hash loader with different color renders as the same color (#602)
    * fix-hash
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- src/helpers/animation.test.ts
+++ src/helpers/animation.test.ts
@@ -7,6 +7,6 @@ describe("createAnimation", () => {
       "0% {left: -35%;right: 100%} 60% {left: 100%;right: -90%} 100% {left: 100%;right: -90%}",
       "my-suffix"
     );
-    expect(name).toEqual("react-spinners-TestLoader-my-suffix");
+    expect(name).toMatch(/^react-spinners-TestLoader-my-suffix/);
   });
 });
