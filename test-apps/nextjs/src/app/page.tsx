db76faaf4f46cd4e61c7307e9401543783a33cca
David Hu <davidhu314@gmail.com>
2025-04-14 16:44:43 -0700

    fix

---
+++ test-apps/nextjs/src/app/page.tsx
@@ -0,0 +1,21 @@
+import * as Loaders from "../../../../src/index";
+
+export default function Home() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        gap: 12,
+      }}
+    >
+      <h1>Test App</h1>
+      <Loaders.BarLoader />
+      <Loaders.BeatLoader />
+      <Loaders.BounceLoader />
+    </div>
+  );
+}
