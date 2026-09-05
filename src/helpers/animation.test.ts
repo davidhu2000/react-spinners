c6cad53f56985762f05fb0b06d5f5f8ce8b68b4d
David Hu <davidhu314@gmail.com>
2026-09-05 16:10:26 -0700

    test: improve coverage reporting

--- src/helpers/animation.test.ts
+++ src/helpers/animation.test.ts
@@ -9,4 +9,19 @@ describe("createAnimation", () => {
     );
     expect(name).toEqual("react-spinners-TestLoader-my-suffix");
   });
+
+  it("should handle a missing stylesheet", () => {
+    const originalCreateElement = document.createElement.bind(document);
+    const createElement = jest.spyOn(document, "createElement").mockImplementation((tagName) => {
+      const element = originalCreateElement(tagName);
+      if (tagName === "style") Object.defineProperty(element, "sheet", { value: null });
+      return element;
+    });
+
+    expect(createAnimation("TestLoader", "", "missing-stylesheet")).toBe(
+      "react-spinners-TestLoader-missing-stylesheet"
+    );
+
+    createElement.mockRestore();
+  });
 });
